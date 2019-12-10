7c3b1c1ca1f126bff212351ece22de4aefbfc9e1
Lotfi Derbali <l.derbali@cgodin.qc.ca>
2019-12-09 20:27:45 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace demo_csharp_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
